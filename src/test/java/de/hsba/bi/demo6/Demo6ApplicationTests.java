ac573c0d1e9291865646aea2c368c9fe33a1fa2d
Benedikt <benedikt.kronhardt@myhsba.de>
2022-08-04 11:23:29 +0200

    initial commit

---
+++ src/test/java/de/hsba/bi/demo6/Demo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsba.bi.demo6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
