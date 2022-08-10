008eae11cc9f7adacacece8123b96a17307317bc
Benedikt <benedikt.kronhardt@myhsba.de>
2022-08-10 11:53:24 +0200

    add template for editing with model var greeting

--- src/main/java/de/hsba/bi/demo6/web/IndexController.java
+++ src/main/java/de/hsba/bi/demo6/web/IndexController.java
@@ -1,12 +1,19 @@
 package de.hsba.bi.demo6.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String index() {
+    //neuer Parameter vom Typ Model: nimmt Daten für den View auf (per model.addAttribute(...))
+    //neuer Parameter mit der Annotation @Requestparam: definiert einen optionalen Request-Parameter namens hello
+    public String index(Model model, @RequestParam(name="hello", required = false) String hello) {
+        if (hello!=null){
+            model.addAttribute("greeting", "Hello, "+hello+ "!");
+        }
         return "index";
     }
 }
