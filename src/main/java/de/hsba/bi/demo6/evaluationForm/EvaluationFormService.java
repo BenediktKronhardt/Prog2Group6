2440f6863649e8733bee4d0fd4db0e616e715fec
Victor Dickert <victor.dickert@myhsba.de>
2022-08-25 11:49:08 +0200

    maximumAmountQuestionShortTen

--- src/main/java/de/hsba/bi/demo6/evaluationForm/EvaluationFormService.java
+++ src/main/java/de/hsba/bi/demo6/evaluationForm/EvaluationFormService.java
@@ -27,42 +27,35 @@ public class EvaluationFormService {
     }
 
 //  Frage zu EvaluationForm hinzufügen
-    public EvaluationForm addQuestion(EvaluationForm evaluationForm, Question question) {
+    public void addQuestion(EvaluationForm evaluationForm, Question question) {
 //   Variable i zum Bestimmen der Id der Question
-     int i=0;
+        int i = 0;
+
+//  Maximale Anzahl an Fragen die gestellt werden können
+        if (evaluationForm.getQuestions().size() < 10) {
+
 
 //     Wenn es noch keine Questions gibt, wird i=1 gesetzt
-        if(evaluationForm.getQuestions().isEmpty()){
-            i=1;
-        }
+            if (evaluationForm.getQuestions().isEmpty()) {
+                i = 1;
+            }
 
 //      Ansonsten ist i um den Wert 1 größer als die Anzahl der Questions
-        else{
-            i=evaluationForm.getQuestions().size()+1;
-        }
+            else {
+                i = evaluationForm.getQuestions().size() + 1;
+            }
 
 
 //      Hier wird i als ID gesetzt
-        question.setId(i);
-
-//      Damit nicht die ID als Wert genommen wird, wird "i" zu "j"
-        int j = 0;
-        j=i;
-
-//      Es kann eine maximale Grenze erstellt werden
-        if (j <= 4){
+            question.setId(i);
 
             evaluationForm.getQuestions().add(question);
         }
-
-//      Gibt den Bogen zurück damit er ausgelesen werden kann
-        return evaluationForm;
     }
-
 //  Alle Evaluationsbögen anzeigen lassen
     public Collection<EvaluationForm> getAll() {
-        return repository.findAll();
-    }
+            return repository.findAll();
+        }
 
 
 }
