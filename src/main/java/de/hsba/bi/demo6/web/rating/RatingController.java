cb32b459eb99b65f9134415b2a7ecc3e63509e3f
Benedikt <benedikt.kronhardt@myhsba.de>
2022-10-01 14:16:11 +0200

    add user to rating

--- src/main/java/de/hsba/bi/demo6/web/rating/RatingController.java
+++ src/main/java/de/hsba/bi/demo6/web/rating/RatingController.java
@@ -6,6 +6,8 @@ import de.hsba.bi.demo6.evaluationForm.EvaluationFormService;
 import de.hsba.bi.demo6.evaluationForm.Question;
 import de.hsba.bi.demo6.rating.RatingService;
 import de.hsba.bi.demo6.user.User;
+import de.hsba.bi.demo6.user.UserAdapterService;
+import de.hsba.bi.demo6.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -31,6 +33,7 @@ public class RatingController {
 
     private final RatingService ratingService;
     private final EvaluationFormService evaluationFormService;
+    private final UserService userService;
 
 //  ratings und der Evaluationsbogen werden als Modelle hinzugefügt
     @GetMapping
@@ -41,12 +44,12 @@ public class RatingController {
     return "ratings/index";
     }
 
-//  Füge ein Rating hinzu. Momentan gibt es für jede Frage einen einzelnen Button zum bewerten, daher muss die Id der Frage in der URL mitgegeben werden.
-    @PostMapping(path = "/{questionId}")
-    public String rate(@PathVariable("id") Long id, Integer score,@PathVariable("questionId") Long questionId){
-        //TODO: User bestimmen
+//  Füge ein Rating hinzu. Momentan gibt es für jede Frage einen einzelnen Button zum bewerten, daher muss die Id der Frage in der URL mitgegeben werden. Auch wird der jeweils eingeloggte User mitgegeben, welcher danach mit der Methode getUserByUsername bestimmt wird.
+    @PostMapping(path = "/{questionId}/{userName}")
+    public String rate(@PathVariable("id") Long id, Integer score,@PathVariable("questionId") Long questionId, @PathVariable("userName") String userName){
         EvaluationForm evaluationForm = getEvaluationForm(id);
-        ratingService.rateWithoutUser(evaluationFormService.findQuestionById(evaluationForm, questionId), score);
+        User user = userService.findUserByName(userName);
+        ratingService.rate(evaluationFormService.findQuestionById(evaluationForm, questionId),user,score);
         return "redirect:/evaluationForms/"+id+"/ratings";
     }
 
